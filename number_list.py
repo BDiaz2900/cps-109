6b94529021491f74ccca037a4ef6b1e295103ad3
BDiaz2900 <150264243+BDiaz2900@users.noreply.github.com>
2024-02-14 09:37:29 -0500

    Add files via upload

---
+++ number_list.py
@@ -0,0 +1,15 @@
+"""
+
+Create a file named numbers_list.py
+Create a list containing at least 6 integers using the Python list data type.
+Get the minimum value in the list using the min() function.
+Get the maximum value in the list using the max() function.
+Print the minimum and maximum values.
+Upload the file to your GitHub account and send me the link.
+"""
+import math
+
+number = [30, 50, 70, 80, 100, 120]
+
+print(f'The maximum number in the list is  {max(number)}.')
+print(f'The minimum number in the list is  {min(number)}.')
